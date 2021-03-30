ecb3c77b5c760b5553e81c5ba9144bff3c072189
tensa132 <c14180033@john.petra.ac.id>
2021-03-30 19:17:35 +0700

    Tab 3 Done

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { FotoService } from '../services/foto.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,41 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  urlImageStorage : string[] = [];
+  imageNameStorage : String[] = [];
 
+  imageStorage = [{}]
+
+  constructor(
+    private afStorage : AngularFireStorage,
+    public fotoService : FotoService
+  ) {}
+
+  async ngOnInit() {
+    await this.fotoService.loadFoto();
+  }
+
+  async ionViewDidEnter() {
+    await this.fotoService.loadFoto();
+    this.tampilkanData();
+  }
+
+  tampilkanData() {
+    this.urlImageStorage = []
+    var refImage = this.afStorage.storage.ref('imgStorage');
+    refImage.listAll()
+    .then((res) => {
+      res.items.forEach((itemRef) => {
+        itemRef.getDownloadURL().then((url) => {
+          this.urlImageStorage.unshift(url)
+          this.imageStorage.unshift({
+            link: url,
+            namaFoto: itemRef.name
+          })
+        })
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 }
