3c330a42b748222205ced4651fbb858343620e7b
tensa132 <c14180033@john.petra.ac.id>
2021-03-30 18:24:39 +0700

    Tab2 Kurang Select

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { FotoService } from '../services/foto.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  urlImageStorage : string[] = [];
 
+  constructor(
+    public fotoService:FotoService,
+    private afStorage : AngularFireStorage
+  ) {
+
+  }
+
+  async ngOnInit() {
+    await this.fotoService.loadFoto();
+  }
+
+  addPhoto() {
+    this.fotoService.addPhoto();
+  }
+  uploadFoto() {
+    this.urlImageStorage = []
+    for (var index in this.fotoService.dataFoto) {
+      const imgFilepath = `imgStorage/${this.fotoService.dataFoto[index].filePath}`
+      this.afStorage.upload(imgFilepath, this.fotoService.dataFoto[index].dataImage).then(() => {
+        this.afStorage.storage.ref().child(imgFilepath).getDownloadURL().then((url) => {
+          this.urlImageStorage.unshift(url);
+          console.log(url);
+        });
+      });
+    }
+    console.log('DONE');
+  }
 }
